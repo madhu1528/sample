0cbe1af0bd01a1acbc0583987dad965a1436210e
madhu1528 <46200214+madhu1528@users.noreply.github.com>
2018-12-28 00:05:57 +0530

    added project to repo on 28/12/2018

---
+++ Empsys/Empsys/SAMPLE.aspx
@@ -0,0 +1,67 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="SAMPLE.aspx.cs" Inherits="Empsys.SAMPLE" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>EMplyoee Details</title>
+</head>
+<body>
+    <form id="form1" runat="server">
+   <h8 >Employee Data Management Using C# & SQL</h8>
+    <div>
+         <table>
+           <tr>
+               <td>EmployeeName:</td>
+               <td> <asp:TextBox ID="txtName" runat="server"></asp:TextBox></td>
+               
+           </tr>
+           <tr>
+               <td>DOB:</td>
+               <td> <asp:TextBox ID="txtDOB" runat="server"></asp:TextBox></td>
+               
+           </tr>
+           <tr>
+               <td>Location:</td>
+               <td> <asp:TextBox ID="txtLocation" runat="server"></asp:TextBox></td>
+               
+           </tr>
+           <tr>
+               <td><asp:Button ID="btnSave" runat="server" Text="Save" Height="26px" OnClick="btnSave_Click" Width="59px" /></td>
+               
+           </tr>
+
+       </table>
+      </div>
+        <div>
+            <asp:HiddenField ID="HiddenField1" runat="server" Value="Insert" />
+            <asp:HiddenField ID="HiddenField2" runat="server" Value="Select" />
+            <asp:Label ID="Label1" runat="server" Text="Label"></asp:Label><br />
+            <asp:GridView ID="GridView1" runat="server" 
+                DataKeyNames="EmpId" OnRowEditing="edit" 
+        OnRowCancelingEdit="canceledit"
+        OnRowDeleting="delete"
+        OnRowUpdating="update"
+        
+         CellPadding="4" ForeColor="#333333" 
+        GridLines="None">
+        <AlternatingRowStyle BackColor="White" />
+        <Columns>
+            <asp:CommandField ShowEditButton="True" />
+            <asp:CommandField ShowDeleteButton="True" />
+        </Columns>
+        <FooterStyle BackColor="#990000" Font-Bold="True" ForeColor="White" />
+        <HeaderStyle BackColor="#990000" Font-Bold="True" ForeColor="White" />
+        <PagerStyle BackColor="#FFCC66" ForeColor="#333333" HorizontalAlign="Center" />
+        <RowStyle BackColor="#FFFBD6" ForeColor="#333333" />
+        <SelectedRowStyle BackColor="#FFCC66" Font-Bold="True" ForeColor="Navy" />
+        <SortedAscendingCellStyle BackColor="#FDF5AC" />
+        <SortedAscendingHeaderStyle BackColor="#4D0000" />
+        <SortedDescendingCellStyle BackColor="#FCF6C0" />
+        <SortedDescendingHeaderStyle BackColor="#820000" /> 
+        </asp:GridView>
+            
+      </div>
+    </form>
+</body>
+</html>
---
+++ Empsys/Empsys/SAMPLE.aspx.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Empsys
+{
+    public partial class SAMPLE : System.Web.UI.Page
+    {
+        static string connectionString = "Data Source=MADHUBABU-PC;Initial Catalog=msdemodb;Persist Security Info=True;User ID=sa;Password=123";
+        SqlConnection connection = new SqlConnection(connectionString);
+        protected void Page_Load(object sender, EventArgs e)
+        {
+        //for git commit
+            if (IsPostBack == false)
+            {
+                gedata();
+            }
+            Label1.Visible = false;
+        }
+
+        protected void btnSave_Click(object sender, EventArgs e)
+        {
+               connection.Open();
+               HiddenField1.Value = "Insert";
+               SqlCommand com = new SqlCommand("EmpEntry", connection);  //Stored Procedure name //creating  SqlCommand  object
+               com.CommandType = CommandType.StoredProcedure;            //here we declaring command type as stored Procedure
+               // /* adding paramerters to  SqlCommand below *\
+               com.Parameters.AddWithValue("@Action", HiddenField1.Value).ToString();//for ing hidden value to preform insert operation
+               com.Parameters.AddWithValue("@EName",txtName.Text.ToString());        //Employee Name
+               com.Parameters.AddWithValue("@DOB ", txtDOB.Text.ToString());         //Employee Dob
+               com.Parameters.AddWithValue("@Location ",txtLocation.Text.ToString());//Employee Location
+               com.ExecuteNonQuery();                     //executing the sqlcommand
+               connection.Close();
+               Label1.Visible = true;
+               Label1.Text = "Records are Submitted Successfully";
+               gedata();
+        }
+
+        public void gedata()
+        {
+            connection.Open();
+            HiddenField1.Value = "Select";
+            SqlCommand com = new SqlCommand("EmpEntry", connection);
+            com.CommandType = CommandType.StoredProcedure;
+            com.Parameters.AddWithValue("@Action", HiddenField1.Value).ToString();
+            SqlDataAdapter da = new SqlDataAdapter(com);
+            DataSet ds = new DataSet();
+            da.Fill(ds);
+            GridView1.DataSource = ds;
+            GridView1.DataBind();
+            connection.Close();
+
+        }
+        protected void edit(object sender, GridViewEditEventArgs e)
+        {
+            GridView1.EditIndex = e.NewEditIndex;
+            gedata();
+        }
+        protected void canceledit(object sender, GridViewCancelEditEventArgs e)
+        {
+            GridView1.EditIndex = -1;
+            gedata();
+        }
+
+        protected void update(object sender, GridViewUpdateEventArgs e)
+        {
+            connection.Open();
+            int id = int.Parse(GridView1.DataKeys[e.RowIndex].Value.ToString());
+            HiddenField1.Value = "Update";
+            SqlCommand com = new SqlCommand("EmpEntry", connection);
+            com.CommandType = CommandType.StoredProcedure;
+            com.Parameters.AddWithValue("@Action", HiddenField1.Value).ToString();
+            com.Parameters.AddWithValue("@EName", ((TextBox)GridView1.Rows[e.RowIndex].Cells[3].Controls[0]).Text.ToString());
+            com.Parameters.AddWithValue("@DOB ", ((TextBox)GridView1.Rows[e.RowIndex].Cells[4].Controls[0]).Text.ToString());
+            com.Parameters.AddWithValue("@Location ", ((TextBox)GridView1.Rows[e.RowIndex].Cells[5].Controls[0]).Text.ToString());
+            com.Parameters.AddWithValue("@EmpId", SqlDbType.Int).Value = id;
+            com.ExecuteNonQuery();
+            connection.Close();
+            GridView1.EditIndex = -1;
+            gedata();
+
+        }
+
+        protected void delete(object sender, GridViewDeleteEventArgs e)
+        {
+            connection.Open();
+            int EmpId = int.Parse(GridView1.DataKeys[e.RowIndex].Value.ToString());
+            HiddenField1.Value = "delete";
+            SqlCommand com = new SqlCommand("EmpEntry", connection);
+            com.CommandType = CommandType.StoredProcedure;
+            com.Parameters.AddWithValue("@Action", HiddenField1.Value).ToString();
+            com.Parameters.AddWithValue("EmpId", SqlDbType.Int).Value = EmpId;
+            com.ExecuteNonQuery();
+            connection.Close();
+            gedata();
+        }
+     }
+}
